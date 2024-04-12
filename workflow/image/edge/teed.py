478eb38a1636bc478b644ef33a9baf83e279ac99
Jonathan <jonathangfischoff@gmail.com>
2024-04-11 23:33:34 -0700

    works

--- workflow/image/edge/teed.py
+++ workflow/image/edge/teed.py
@@ -1,4 +1,5 @@
 import fal
+import PIL.Image
 from fal.toolkit import (
     FAL_REPOSITORY_DIR,
     Image,
@@ -27,9 +28,8 @@ class TeedOutput(BaseModel):
     image: Image = Field(description="The edge map.")
 
 
-def run_teed(
-    input: TeedInput,
-) -> TeedOutput:
+@fal.cached
+def load_teed():
     clone_repository(
         "https://github.com/xavysp/TEED.git",
         commit_hash="5e074acb7a0d39f81356b3d08801e2bb7526e850",
@@ -41,13 +41,9 @@ def run_teed(
     if str(TEED_REPO_PATH) not in sys.path:
         sys.path.insert(0, str(TEED_REPO_PATH))
 
-    import numpy as np
-    import PIL
     import torch
     from ted import TED
-    from torchvision.transforms import ToPILImage
 
-    # cpu is faster
     device = torch.device("cpu")
     model = TED().to(device)
     checkpoint_path = download_model_weights(CHECKPOINT)
@@ -56,56 +52,60 @@ def run_teed(
 
     model.eval()
 
+    return model, device
+
+
+def run_teed(
+    input: TeedInput,
+    model,
+    device,
+) -> TeedOutput:
+    import numpy as np
+    import PIL
+    import torch
+    from utils.img_processing import image_normalization
+
+    # cpu is faster in my tests
     with torch.no_grad():
         image = read_image_from_url(input.image_url)
-        image = image.convert("RGB")
-
         # resize to the nearest multiple of 8
         width, height = image.size
-        print("original image shape", width, height)
-        new_width = width - (width % 8)
-        new_height = height - (height % 8)
+        new_width = (width + 7) // 8 * 8
+        new_height = (height + 7) // 8 * 8
         if new_width != width or new_height != height:
             image = image.resize((new_width, new_height), PIL.Image.LANCZOS)
 
         # convert to pytorch tensor
         image = (
-            (torch.tensor(np.array(image)).permute(2, 0, 1).float() / 255.0)
+            (torch.tensor(np.array(image)).permute(2, 0, 1).float())
             .unsqueeze(0)
             .to(device)
         )
-        print("image shape", image.shape)
 
-        preds = model(image)
-        output_tensor = preds[-1]
-        print("output_tensor shape", output_tensor.shape)
+        # subtract the pixel mean
+        mean_rgb = [123.68, 116.779, 103.939]
+        image = image - torch.tensor(mean_rgb).view(1, 3, 1, 1).to(device)
 
-        assert len(output_tensor.shape) == 4, output_tensor.shape
+        # put in bgr order
+        image = image[:, [2, 1, 0], :, :]
 
-        image_vis = torch.sigmoid(output_tensor)
-        image_vis = 1.0 - image_vis
-        detected_map = ToPILImage()(image_vis[0] * 255.0)
+        # keep as 0-255 because this model is totally unhinged
+        pred = model(image)[-1][0, 0]
+        pred = torch.sigmoid(pred).cpu().detach().numpy()
+        pred = np.uint8(image_normalization(pred))
+        detected_map = PIL.Image.fromarray(pred)
+        if detected_map.size != (width, height):
+            detected_map = detected_map.resize(
+                (width, height), PIL.Image.LANCZOS
+            ).convert("RGB")
 
-        detected_map = detected_map.resize((width, height), PIL.Image.LANCZOS).convert(
-            "RGB"
-        )
-
-        print("detected_map shape", detected_map.size)
-
-        # resize to original size
-
-    result = TeedOutput(image=Image.from_pil(detected_map))
-
-    print("result", result)
-
-    return result
+    return TeedOutput(image=Image.from_pil(detected_map))
 
 
 @fal.function(
     requirements=[
         "Pillow==10.3.0",
         "torch==2.2.2",
-        "torchvision",
         "numpy==1.26.4",
         "opencv-python==4.9.0.80",
         "kornia==0.7.2",
@@ -120,7 +120,8 @@ def run_teed(
 def run_teed_on_fal(
     input: TeedInput,
 ) -> TeedOutput:
-    return run_teed(input)
+    model, device = load_teed()
+    return run_teed(input, model, device)
 
 
 if __name__ == "__main__":
